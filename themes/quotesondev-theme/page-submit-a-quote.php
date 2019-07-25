c86324833b169cc7dd9b93055ac24a789fee051b
fyeung21 <yeung.fiona@hotmail.com>
2019-07-24 19:34:52 -0700

    added new pages and custom php files

--- themes/quotesondev-theme/page-submit-a-quote.php
+++ themes/quotesondev-theme/page-submit-a-quote.php
@@ -5,7 +5,7 @@
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'submit-quote' ); ?>
 
 			<?php endwhile; ?>
 
