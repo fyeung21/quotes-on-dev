6dd5cb5538dc6adb0335306b98baac1eb66973e9
fyeung21 <yeung.fiona@hotmail.com>
2019-08-01 15:47:33 -0700

    styled posts viewed in archive type

--- themes/quotesondev-theme/template-parts/content.php
+++ themes/quotesondev-theme/template-parts/content.php
@@ -1,18 +1,25 @@
-<?php
-/**
- * Template part for displaying posts.
- *
- * @package QOD_Starter_Theme
- */
-
-?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
+		<div class="post-content">
+			<?php the_excerpt(); ?>
+		</div>
+	</header>
 
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+	<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">- ', esc_url(get_permalink())), '</a></h2>'); ?>
+
+	<?php $source = get_post_meta(get_the_id(), "_qod_quote_source", true);
+	$source_url = get_post_meta(get_the_id(), "_qod_quote_source_url", true); ?>
+
+	<?php if ($source && $source_url) : ?>
+
+		<span class="quote-source"><a class="quote-source-url" href="<?php echo $source_url ?>"><?php echo $source ?></a></span>
+
+	<?php elseif ($source) : ?>
+		<span><?php echo $source ?></span>
+
+	<?php else : ?>
+		<span> </span>
+
+	<?php endif; ?>
+
+</article>
